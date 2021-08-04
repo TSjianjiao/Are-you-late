c026595da018044fbbfd8c906f9381c897c6f3bb
xuhang <929175050@qq.com>
2021-08-04 08:47:45 +0800

    /

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ const watchTS = () => {
 function compile() {
   const compiled = gulp.src('./src/**/*.ts')
     .pipe(alias(tsProject.config))
-    .pipe(tsProject());
+    .pipe(tsProject())
 
   return compiled.js
     .pipe(gulp.dest('build/'))
