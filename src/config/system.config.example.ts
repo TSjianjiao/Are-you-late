bd09404380aacf2d5214f8dd39238017c123ce9e
oliver.xu <929175050@qq.com>
2021-08-28 08:12:24 +0800

    /

---
+++ src/config/system.config.example.ts
@@ -0,0 +1,30 @@
+const runOnPro = process.env.NODE_ENV === 'production'
+export interface SystemConfigInterface {
+  wsPath: string
+  httpPath: string
+  verifyKey:  number
+  bot_qq: string
+  group_qq: string
+  admin_qq: string
+  yuliu_qq: string
+}
+
+const SystemConfig:SystemConfigInterface  = {
+  /** 服务器地址 */
+  wsPath: '',
+  /** http地址 */
+  httpPath: '',
+  /** 鉴权key */
+  verifyKey:  1,
+  /** bot的qq号 */
+  bot_qq: '',
+  /** qq群号 */
+  group_qq: '',
+  /** 管理员qq */
+  admin_qq: '',
+  /** 摆子哥qq */
+  yuliu_qq: ''
+}  
+
+export default SystemConfig
+ 
